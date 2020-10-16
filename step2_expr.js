da1406a8e331e97a6311e482da5e0a1564136fa7
Yamaguchi Hiromitsu <yamagame@mac.com>
2020-10-16 14:22:47 +0900

    update EBNF

--- step2_expr.js
+++ step2_expr.js
@@ -1,6 +1,7 @@
 // add     = mul ("+" mul | "-" mul)*
 // mul     = num ("*" num | "/" num)*
-// num     = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"
+// num     = digit (digit)*
+// digit   = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"
 
 module.exports = function(token)
 {
